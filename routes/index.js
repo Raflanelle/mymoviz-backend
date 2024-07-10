fc3b1133d6bef06ffd4a67e3ad11af5fa0dda114
raphaelle <raflanelle@hotmail.com>
2024-07-10 16:17:38 +0200

    mymoviz premier commit backend

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+const fetch = require('node-fetch');
+
+const TOKEN = process.env.IMBD_TOKEN;
+
+const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: TOKEN
+    }
+};
+
+router.get('/movies', (req, res) => {
+    fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc', options)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.results)
+            res.json({ movies: data })
+        })
+  })
+
+
+
+module.exports = router;
